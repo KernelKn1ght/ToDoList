a8a006370b2f09f4df40438b52726796cbf814fb
Samuel Silva <sdeandrade66@gmail.com>
2025-08-19 16:47:18 -0300

    feat: add menu principal com opções

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,10 @@
 package org.example;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
+        ArrayList<Tarefa> lista = new ArrayList<>();
        Scanner sc = new Scanner(System.in);
        int opcao;
 
@@ -15,9 +17,19 @@ public class Main {
            System.out.println("0 - Sair ");
            System.out.println("Escolha: ");
            opcao = sc.nextInt();
+           sc.nextLine(); // quebrar a linha
 
-           //  por enquanto, só imprime
-           System.out.println();
+           switch (opcao) {
+               case 1:
+                   System.out.println("Descrição da Tarefa");
+                   String desc = sc.nextLine();
+                   lista.add(new Tarefa(desc));
+                   System.out.println("Tarefa Adicionada com Sucesso !");
+                   break;
+               default :
+                   System.out.println("Opção Inválida(por enquanto, apenas)");
+           }
+
        } while (opcao != 0);
 
        sc.close();
